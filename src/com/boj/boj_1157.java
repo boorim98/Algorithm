a59387d3c2f043772342d04875429e8aab1242e5
boorim98 <bhuyugirl@gmail.com>
2021-05-18 16:15:37 +0900

    Add: boj_1157

---
+++ src/com/boj/boj_1157.java
@@ -0,0 +1,46 @@
+package com.boj;
+import java.util.*;
+
+public class boj_1157 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.nextLine();
+
+        int[] wordArray = new int[26];
+
+        for (int i = 0; i < word.length(); i++) {
+            char w = word.charAt(i);
+            if (Character.isUpperCase(w)) {
+                wordArray[w-65]++;
+            }
+            else {
+                wordArray[w-97]++;
+            }
+        }
+
+        int max = 0;
+        boolean many = true;
+        for (int i = 1; i < wordArray.length; i++) {
+            if (wordArray[max] <= wordArray[i]) {
+                max = i;
+            }
+        }
+
+        for (int i = 0; i < wordArray.length; i++) {
+            if (i == max) {
+                continue;
+            }
+            else {
+                if (wordArray[i] == wordArray[max]) {
+                    many = false;
+                }
+            }
+        }
+
+        if (many) System.out.println((char)(max+65));
+        else System.out.println("?");
+
+    }
+}
+
