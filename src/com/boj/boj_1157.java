d8a0c484c9869ca56978b6c942b4e10fbb8b058f
boorim98 <bhuyugirl@gmail.com>
2021-05-21 15:09:50 +0900

    Fix: boj_1157

--- src/com/boj/boj_1157.java
+++ src/com/boj/boj_1157.java
@@ -19,7 +19,7 @@ public class boj_1157 {
             }
         }
 
-        int max = 0;
+        /*int max = 0;
         boolean many = true;
         for (int i = 1; i < wordArray.length; i++) {
             if (wordArray[max] <= wordArray[i]) {
@@ -39,7 +39,22 @@ public class boj_1157 {
         }
 
         if (many) System.out.println((char)(max+65));
-        else System.out.println("?");
+        else System.out.println("?");*/
+
+        int max = -1;
+        char ch = '?';
+
+        for (int i = 0; i < 26; i++) {
+            if (wordArray[i] > max) {
+                max = wordArray[i];
+                ch = (char) (i + 65); // 대문자로 출력해야하므로 65를 더해준다.
+            }
+            else if (wordArray[i] == max) {
+                ch = '?';
+            }
+        }
+
+        System.out.println(ch);
 
     }
 }
