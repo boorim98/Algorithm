ebd1a4588963c504d4e0059358deb2e9c96b5d8f
boorim98 <bhuyugirl@gmail.com>
2021-05-25 17:25:35 +0900

    Fix: boj_2798

--- src/com/boj/boj_2798.java
+++ src/com/boj/boj_2798.java
@@ -13,39 +13,19 @@ public class boj_2798 {
             arr[i] = scanner.nextInt();
         }
 
-        /*잘못 생각한 코드*/
+        int sum = 0;
+        int max = 0;
+        for (int i = 0; i < arr.length - 2; i++){
+            for (int j = i + 1; j < arr.length - 1; j++) {
+                for (int k = j + 1; k < arr.length; k++)  {
+                    sum = arr[i] + arr[j] + arr[k];
 
-        boolean[] visited = new boolean[arr.length];
-        int result = combination(arr, visited, 0, arr.length, 3, m, m, 0);
-
-        System.out.println(result);
-    }
-
-    static int combination(int[] arr, boolean[] visited, int start, int n, int r, int m, int close, int result) {
-
-        if (r == 0) {
-            int sum = 0;
-            for (int i = 0; i < arr.length; i++) {
-                if (visited[i]){
-                    sum += arr[i];
-                    //System.out.println("arr = " + arr[i]);
+                    if (m >= sum && sum > max) {
+                        max = sum;
+                    }
                 }
             }
-            //System.out.println("sum = " + sum);
-            if (sum <= m && m - sum < close) {
-                close = m - sum;
-                System.out.println("sum = " + sum);
-
-                result = sum;
-                System.out.println("result = " + result);
-            }
         }
-        for (int i = start; i < n; i++) {
-            visited[i] = true;
-            combination(arr, visited, i + 1, n, r - 1, m, close, result);
-            visited[i] = false;
-        }
-
-        return result;
+        System.out.println(max);
     }
 }
