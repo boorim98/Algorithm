b36c7e3f55b01966ed1ef44f071994ee0d3e5afa
boorim98 <bhuyugirl@gmail.com>
2021-07-14 22:08:43 +0900

    Fix: boj_1931

--- src/com/boj/boj_1931.java
+++ src/com/boj/boj_1931.java
@@ -14,18 +14,13 @@ public class boj_1931 {
             time[i][1] = scanner.nextInt();
         }
 
-        Arrays.sort(time, new Comparator<int[]>() {
+        Arrays.sort(time, (o1, o2) -> {
 
-            @Override
-            public int compare(int[] o1, int[] o2) {
-
-                if(o1[1] == o2[1]) {
-                    return o1[0] - o2[0];
-                }
-
-                return o1[1] - o2[1];
+            if(o1[1] == o2[1]) {
+                return o1[0] - o2[0];
             }
 
+            return o1[1] - o2[1];
         });
 
         int count = 0;
