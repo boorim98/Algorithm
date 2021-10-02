8c9ac35063b59d0998f5e273c989a2a5d656345a
boorim98 <bhuyugirl@gmail.com>
2021-10-02 16:52:09 +0900

    Fix: boj_15650 주석

--- src/com/boj/boj_15650.java
+++ src/com/boj/boj_15650.java
@@ -3,6 +3,11 @@ package com.boj;
 import java.util.*;
 import java.io.*;
 
+/*
+N과 M (1)과 차이점 : 고른 수열은 오름차순이어야 한다.
+따라서 입력을 4 4로 해주면 1 2 3 4만 출력되어야 한다.
+*/
+
 public class boj_15650 {
 
     static int N, M;
@@ -19,7 +24,7 @@ public class boj_15650 {
 
         back(1, 0);
     }
-    public static void back(int idx, int count){
+    public static void back(int idx, int count){ //오름차순으로 수열 만들기 위하여 idx라는 인자 하나 추가
         if (count == M) {
             for (int i = 0; i < M; i++) {
                 System.out.print(arr[i] + " ");
@@ -28,7 +33,7 @@ public class boj_15650 {
             return;
         }
 
-        for (int i = idx; i <= N; i++) {
+        for (int i = idx; i <= N; i++) { //오름차순을 위하여 숫자 탐색 범위를 idx부터 해야함(전 수보다 커야하니까)
             arr[count] = i;
             back(i + 1, count + 1);
         }
