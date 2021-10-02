c1681010774f51efedb8f524a16e1788fe04fd8c
boorim98 <bhuyugirl@gmail.com>
2021-10-02 16:04:41 +0900

    Fix: boj_15649 주석

--- src/com/boj/boj_15649.java
+++ src/com/boj/boj_15649.java
@@ -15,13 +15,13 @@ public class boj_15649 {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-        arr = new int[10];
-        check = new boolean[10];
+        arr = new int[10]; //만든 조합을 저장하는 배열
+        check = new boolean[10]; //중복이 허용 안되므로 수가 사용되었는지 확인하는 boolean 배열
 
-        back(0);
+        back(0); //배열의 길이로 인자
     }
     public static void back(int count){
-        if (count == m) {
+        if (count == m) { //count가 m이랑 같으면 배열 출력
             for (int i = 0; i < m; i++){
                 System.out.print(arr[i] + " ");
             }
@@ -29,12 +29,12 @@ public class boj_15649 {
             return;
         }
 
-        for (int i = 1; i <= n; i++){
-            if (!check[i]) {
-                arr[count] = i;
-                check[i] = true;
-                back(count + 1);
-                check[i] = false;
+        for (int i = 1; i <= n; i++){ //if문에 해당하지 않는다면 아직 배열에 수를 넣어야 함
+            if (!check[i]) { //아직 쓰이지 않은 수이면
+                arr[count] = i; //배열에 수 저장
+                check[i] = true; //그 수 쓰였다고 boolean 배열에 저장
+                back(count + 1); //count 하나 늘려서 함수 다시 실행
+                check[i] = false; //return 후 다시 원래대로 돌려놓기
             }
         }
     }
